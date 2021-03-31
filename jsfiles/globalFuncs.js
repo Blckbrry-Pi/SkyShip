9944ba04ebe118d44b90f8c2d1ed655c00e93e79
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-03-31 12:01:07 -0400

    Shuffle around locations of some functions

---
+++ jsfiles/globalFuncs.js
@@ -0,0 +1,111 @@
+function timeStep() {
+  timeMult = getTimeMult(runner.pos.x, runner.pos.y, attractors);
+  
+  
+  if (runner.connectedAttractor.index !== -1) {
+    //onConnect(timeMult);
+  }
+  attractors.forEach(
+    element => {
+      if (element.collided(runner.pos.x, runner.pos.y)) state = newState(states.dead);
+    }
+  );
+  obstacles.forEach(
+    element => {
+      if (element.pointInObstacle(runner.pos)) state = newState(states.dead);
+    }
+  );
+}
+
+function starStep() {
+  rotateStars(timeMult);
+}
+
+function attractorStep() {
+  attractors.forEach(
+    element => {
+      element.rotStep(timeMult);
+    }
+  );
+}
+
+function runnerStep() {
+  if (mouseIsPressed) {
+    if (!mouseWasPressed) {
+      runner.onMouseDown(attractors);
+    }
+  } else {
+    if(mouseWasPressed) {
+      runner.onMouseRelease();
+      onDisconnect();
+    }
+  }
+  mouseWasPressed = mouseIsPressed;
+  
+  runner.updateSpringLength(timeMult);
+  
+  runner.doPhysicsStep(attractors, zippers, timeMult);
+}
+
+
+
+
+
+function updateCamera() {
+  viewScale = 1.5;
+  viewTranslation = new p5.Vector(runner.pos.x - width / 2 / viewScale, runner.pos.y - height / 2 / viewScale);
+  
+  globalMouse.scale = viewScale;
+  globalMouse.translation = viewTranslation;
+}
+
+function drawBase() {
+  starryBackground();
+  obstacles.forEach(
+    element => {
+      element.draw();
+    }
+  );
+}
+
+
+
+
+
+
+
+function drawAttractors() {
+  drawingContext.save();
+  drawingContext.beginPath();
+  for (let i = 0; i < attractors.length; i++) {
+    attractors[i].drawBounds();
+  }
+  for (let i = attractors.length - 2; i > 0; i--) {
+    attractors[i].drawBounds();
+  }
+  drawingContext.clip();
+  drawBase();
+  drawingContext.restore();
+
+  attractors.forEach(
+    element => {
+      element.draw();
+    }
+  );
+}
+
+
+
+function drawScene() {
+  drawBase();
+
+  zippers.forEach(
+    element => {
+      element.draw();
+    }
+  );
+  
+  drawAttractors();
+
+  runner.draw();
+}
