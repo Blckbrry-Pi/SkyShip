c5f288c152a93cec2a4a692cb16827c7bdf49bea
Skyler Calaman <54462713+Blckbrry-Pi@users.noreply.github.com>
2021-04-01 21:13:52 -0400

    Add saving of unlocked levels to localStorage

--- sketch.js
+++ sketch.js
@@ -9,16 +9,46 @@ function setup() {
   initStars(width * height / 320);
 
   state = newState(states.menu);
+
+  loadUnlocked();
+}
+
+function loadUnlocked() {
+  try {
+    let unlockedInStorage = JSON.parse(localStorage.getItem('unlockedLevels'));
+    if (unlockedInStorage.length === 0) return;
+
+    unlocked = [];
+
+    for (let i = 0; i < unlockedInStorage.length; i++)
+      if (levelsDisplayed.includes(unlockedInStorage[i]))
+        unlocked.push(unlockedInStorage[i]);
+      
+    
+  }
+  catch (e) {}
 }
 
 
 function draw() {
   doStateLoop(state);
   state.stateTimer++;
+
+  if (!(frameCount % 150)) saveUnlocked();
 }
 
-function keyPressed() {
-  if (keyCode === 32 || keyCode == 80) {
-    isLooping() ? noLoop() : loop();
-  }
+function saveUnlocked() {
+  let unlockedInStorage = JSON.parse(localStorage.getItem('unlockedLevels'));
+
+  if (!Array.isArray(unlockedInStorage)) unlockedInStorage = [];
+
+  let unlockedToStorage = [...unlocked];
+
+  for (let i = 0; i < unlockedInStorage.length; i++)
+    if (!unlockedToStorage.includes(unlockedInStorage[i]))
+      unlockedToStorage.push(unlockedInStorage[i]);
+  
+  unlockedToStorage.sort((a, b) => a - b);
+
+  localStorage.setItem('unlockedLevels', JSON.stringify(unlockedToStorage));
 }
